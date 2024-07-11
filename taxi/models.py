208340b9b4dd2a6110656983c2304f7410e1f67d
Oleh Deshko <kochaminny@gmail.com>
2024-07-11 13:42:11 +0300

    sol

--- taxi/models.py
+++ taxi/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", args=[str(self.id)])
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
-
-
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def get_absolute_url(self):
+        return reverse("taxi:car-detail", args=[str(self.id)])
